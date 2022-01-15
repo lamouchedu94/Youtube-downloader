11898f09d48614b46996924d8cb8810a0f3cf8f7
lamouchedu94 <lamouchedu54@gmail.com>
2022-01-15 17:44:46 +0100

    modif

--- updater.py
+++ updater.py
@@ -2,7 +2,6 @@ import os
 import time
 try :
     os.system("git clone https://github.com/lamouchedu94/Youtube-downloader-FR Downloader")
-    os.system("rm /Downloader/updater.py")
     print("Mise à jour réussie !")
     time.sleep(5)
 except :
