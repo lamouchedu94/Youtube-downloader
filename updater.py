01a5020b9a3edd3415bd89b6bc616059772d37c6
lamouchedu94 <lamouchedu54@gmail.com>
2022-01-29 16:21:44 +0100

    reset

--- updater.py
+++
@@ -1,7 +0,0 @@
-import os
-import time
-
-os.system("git clone https://github.com/lamouchedu94/Youtube-downloader-FR update")
-print("pour garder vos paramètres ne supprimez pas le .json orginal")
-time.sleep(5)
-
